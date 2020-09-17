8f11cd2eddf15f28b224f00b82e30685dccfed8b
olmoshbtn <2120@holbertonschool.com>
2020-09-17 18:45:54 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int e = '\n';
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar(e);
+return (0);
+}
