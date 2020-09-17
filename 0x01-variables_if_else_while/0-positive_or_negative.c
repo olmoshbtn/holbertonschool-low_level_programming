1c0a50203adbbb175a63f6bdae0b43eb791bb36a
olmoshbtn <2120@holbertonschool.com>
2020-09-17 16:43:55 +0000

    delete indent

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    {
-      printf("%i is positive\n", n);
-    }
-  else if (n < 0)
-    {
-      printf("%i is negative\n", n);
-    }
-  else
-    {
-      printf("0 is zero\n");
-    }
-  return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+else
+{
+printf("0 is zero\n");
+}
+return (0);
 }
