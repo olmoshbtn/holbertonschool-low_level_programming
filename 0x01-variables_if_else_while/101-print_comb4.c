dcc400087c37a6d30899db3ee36409eb7a3ee75e
olmoshbtn <2120@holbertonschool.com>
2020-09-18 03:24:28 +0000

    100 and 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = '0'; i <= '7'; i++)
+{
+for (j = i + 1; j <= '8'; j++)
+{
+for (k = j + 1; k <= '9'; k++)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (k < '9' || j < '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
