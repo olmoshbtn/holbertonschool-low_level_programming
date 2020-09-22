668cf3458bebf9f2fbc314fd68ac918a53572531
olmoshbtn <2120@holbertonschool.com>
2020-09-22 23:05:39 +0000

    excercise 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @r: no olvidar
+ * Return: Always 0.
+ */
+int print_last_digit(int r)
+{
+	int lastD;
+
+	lastD = r % 10;
+	if (lastD < 0)
+	{
+		return (lastD * (-1));
+	}
+	return (lastD);
+}
