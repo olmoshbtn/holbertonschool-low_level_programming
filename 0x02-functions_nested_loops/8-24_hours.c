516e0d894c7150299f9d1438e8d806ee638eceb3
olmoshbtn <2120@holbertonschool.com>
2020-09-23 19:01:46 +0000

    exercise 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int A;
+int B;
+
+for (A = 00; A < 24; A++)
+{
+for (B = 00; B < 60; B++)
+{
+_putchar((A / 10) + '0');
+_putchar((A % 10) + '0');
+_putchar(':');
+_putchar((B / 10) + '0');
+_putchar((B % 10) + '0');
+_putchar('\n');
+}
+}
+}
