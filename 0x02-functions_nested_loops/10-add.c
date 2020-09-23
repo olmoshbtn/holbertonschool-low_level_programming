d4872dc81f9cd64b1e4549566ab5f4e91c0ae7bc
olmoshbtn <2120@holbertonschool.com>
2020-09-23 23:43:09 +0000

    exercise 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @a: first number
+ * @b: second number
+ * Return: result suma.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
