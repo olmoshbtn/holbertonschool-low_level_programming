be6ef81636b7e98ad0ee839c0c35bd9c8ac2bed4
olmoshbtn <2120@holbertonschool.com>
2020-09-24 15:47:45 +0000

    edit mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Entry pointchecks for a digit (0 through 9)
+ * @a: first_variable
+ * @b: second_variable
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+}
