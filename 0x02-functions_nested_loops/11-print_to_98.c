7aeae1278f9c5a6b9a135211ac0b0da80a24ad2e
olmoshbtn <2120@holbertonschool.com>
2020-09-24 01:24:40 +0000

    delete space

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,16 @@
  */
 void print_to_98(int n)
 {
-  if (n >= 98)
-    {
-      for ( ; n > 98; n--)
-	printf("%d, ", n);
-      printf("%d\n", n);
-    }
-  else
-    {
-      for ( ; n < 98; n++)
-	printf("%d, ", n);
-      printf("%d\n", n);
-    }
+if (n >= 98)
+{
+for ( ; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", n);
+}
+else
+{
+for ( ; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", n);
+}
 }
