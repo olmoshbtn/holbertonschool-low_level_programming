c3a4241c655982f0935a79b7c685b5f7ff66791b
olmoshbtn <2120@holbertonschool.com>
2020-09-24 19:44:24 +0000

    exercise 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n:  number of times the character _ should be printed.
+ * Return: Void.
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (; n >= 1; n--)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
