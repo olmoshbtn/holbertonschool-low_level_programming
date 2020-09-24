904861b975a1d8ce74b44f4745e888cde4e58a36
olmoshbtn <2120@holbertonschool.com>
2020-09-24 13:53:12 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * int _isupper - Entry point
+ * @c: no_olvidar
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
+        return (0);
+}
