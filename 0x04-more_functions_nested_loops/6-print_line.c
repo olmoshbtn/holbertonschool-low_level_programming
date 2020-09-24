1c49a03f228ad8bc17df22b14a0ef3d78ec5dbdf
olmoshbtn <2120@holbertonschool.com>
2020-09-24 19:58:58 +0000

    re-write

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@
 
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (; n >= 1; n--)
-{
-_putchar('_');
-}
-}
-_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (; n >= 1; n--)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }
