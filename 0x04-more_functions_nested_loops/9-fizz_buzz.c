07e288291d4f531882775883ec9ab64a5020f522
olmoshbtn <70914717+olmoshbtn@users.noreply.github.com>
2020-09-25 07:10:11 -0300

    Update 9-fizz_buzz.c
    mofied web

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,35 +8,33 @@
 
 int main(void)
 {
-  int i;
+	int i;
 
-  for (i = 1; i <= 100; i++)
-    {
-      if ((i % 3 == 0) && (i % 5 == 0))
-	{
-	  printf("FizzBuzz");
-	}
-      else if (i % 3 == 0)
-	{
-	  printf("Fizz");
-	}
-      else if (i % 5 == 0)
-	{
-	  printf("Buzz");
-	}
-      else
-	{
-	  printf("%d", i);
-	}
+  	for (i = 1; i <= 100; i++)
+    	{
+      		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+	  		printf("FizzBuzz");
+		}
+      		else if (i % 3 == 0)
+		{
+	  		printf("Fizz");
+		}
+      		else if (i % 5 == 0)
+		{
+	  		printf("Buzz");
+		}
+      		else
+		{
+	  		printf("%d", i);
+		}
 
-      if (i < 100)
-	{
-	  putchar(' ');
-	}
-      else
-	{
-	  putchar('\n');
-	}
-    }
-  return (0);
+      		if (i < 100)
+		{
+	  	putchar(' ');
+		}
+      		else
+	  	putchar('\n');
+  	}
+	return (0);
 }
