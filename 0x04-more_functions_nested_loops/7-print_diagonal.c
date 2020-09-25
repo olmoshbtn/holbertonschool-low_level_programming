881d01b5f03cfb439f95a0d2abf95c125d4e041d
olmoshbtn <2120@holbertonschool.com>
2020-09-25 01:48:32 +0000

    modifc. 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,17 @@
 
 void print_diagonal(int n)
 {
-  if (n <= 0)
+  int fil;
+  int col;
+
+  for (fil = 0; fil < n; ++fil)
     {
+      for (col = 0; col < fil; ++col)
+	_putchar(' ');
+      _putchar('\\');
       _putchar('\n');
     }
-  else
-    {
-      int fil;
-      int col;
-      for (fil = 0; fil < n; ++fil)
-	{
-	  for (col = 0; col < fil; ++col)
-	    {
-	      _putchar(' ');
-	    }
-	  _putchar('\\');
-	  _putchar('\n');
-	}
-    }
+
+  if (n <= 0)
+    _putchar('\n');
 }
