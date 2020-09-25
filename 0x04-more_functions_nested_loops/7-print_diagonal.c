358ec4ea9b4b4bfc2e933a8b6e3480e0a8475a36
olmoshbtn <2120@holbertonschool.com>
2020-09-25 00:52:46 +0000

    add brackets

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,13 @@
 void print_diagonal(int n)
 {
   if (n <= 0)
-    _putchar('\n');
-
+    {
+      _putchar('\n');
+    }
   else
     {
       int fil;
       int col;
-      
       for (fil = 0; fil < n; ++fil)
 	{
 	  for (col = 0; col < fil; ++col)
