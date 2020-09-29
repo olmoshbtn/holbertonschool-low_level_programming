ae06107d5ba9eeddb91f78b312d527b8bd719e78
olmoshbtn <2120@holbertonschool.com>
2020-09-29 19:49:00 +0000

    delete

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: string
+ *
+ * Return: Alway 0.
+ */
+
+void rev_string(char *s)
+{
+	if (str)
+	{
+char *end = str + 
+	}
+}
