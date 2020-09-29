bdc8471be3b65caeeca175c156b328de41145e04
olmoshbtn <2120@holbertonschool.com>
2020-09-29 17:51:37 +0000

    exercise 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - func. that prints a string, in reverse, followed by a new line
+ * @s: string
+ *
+ * Return: Alway 0.
+ */
+
+void print_rev(char *s)
+{
+	char *psz = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	while (s >= psz)
+	{
+		_putchar(*(s - 1));
+		s--;
+	}
+	_putchar('\n');
+}
