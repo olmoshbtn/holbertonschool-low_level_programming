2b0198b1c8c8f737a0e614d4efa26ec544a1f446
olmoshbtn <2120@holbertonschool.com>
2020-09-30 18:59:30 +0000

    exercise 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - function that copies the string pointed
+ * @dest: pointer
+ * @src: pointer
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
