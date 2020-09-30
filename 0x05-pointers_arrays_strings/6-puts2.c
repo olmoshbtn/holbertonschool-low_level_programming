dac62a1f515dd3830330c0a8dc5f47aaa56c51fe
olmoshbtn <2120@holbertonschool.com>
2020-09-30 00:34:02 +0000

    exercise 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - function prints character...
+ * @str: string...
+ *
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != 0)
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
