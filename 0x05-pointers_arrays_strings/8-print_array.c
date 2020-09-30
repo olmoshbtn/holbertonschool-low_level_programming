91fae26a91b3a8055de2d829a9e35873097dffba
olmoshbtn <2120@holbertonschool.com>
2020-09-30 03:22:23 +0000

    exercise 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,16 +13,10 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i <= n; i++)
+	while (i < n - 1)
 	{
-		if (i != n - 1)
-		{
-			printf("%d, ", *(a + i));
-		}
-		else
-		{
-			printf("%d", *(a + i));
-		}
+		printf("%d, ", *(a + i));
+		i++;
 	}
-	printf("\n");
+	printf("%d \n", *(a + i));
 }
