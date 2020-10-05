f1c1a7ace14f21f2e398f64d7ba52f983ecdf017
olmoshbtn <2120@holbertonschool.com>
2020-10-05 19:23:17 +0000

    exercise 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,11 +17,11 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-			{
 				break;
-			}
-			if (s[i] == accept[j])
-				break;
+		}
+		if (s[i] != accept[j])
+		{
+			break;
 		}
 	}
 	return (i);
