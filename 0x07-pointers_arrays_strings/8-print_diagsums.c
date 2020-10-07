0d1b960900d9f54fc14c8e67fea92a64a4c8407c
olmoshbtn <2120@holbertonschool.com>
2020-10-07 01:12:07 +0000

    exercise 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	int sumd1 = 0;
 	int sumd2 = 0;
 
-	for (loc = 0, row = 0; row < size; ++row, loc += size + 1)
+	for (loc = a, row = 0; row < size; ++row, loc += size + 1)
 	{
 		sumd1 += *loc;
 	}
