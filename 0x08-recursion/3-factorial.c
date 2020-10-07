c4391939af411ca004004e613eee293f8e5b6f8e
olmoshbtn <2120@holbertonschool.com>
2020-10-07 21:12:57 +0000

    exercise 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,12 @@
  */
 int factorial(int n)
 {
-	while (n > 0)
+	if (n <= 0)
 	{
-		return (n * factorial(n - 1));
+		if  (n == 0)
+			return (1);
+		else
+			return (-1);
 	}
-			if (n == 0)
-				return (1);
-			else
-				return (-1);
+	return (n * factorial(n - 1));
 }
