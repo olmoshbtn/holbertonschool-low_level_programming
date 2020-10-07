a09ed689ea27c7c989df7bd980b8ae23f02811e0
olmoshbtn <2120@holbertonschool.com>
2020-10-07 00:43:30 +0000

    fix exercise 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 {
 	int row, col;
 
-	for (row = 0; row < 0; row++)
+	for (row = 0; row < 0; ++row)
 	{
-		for (col = 0; col < 8; col++)
+		for (col = 0; col < 8; ++col)
 		{
 			_putchar(a[row][col]);
 		}
