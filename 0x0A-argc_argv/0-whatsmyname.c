7b92b0c9ac037d867832135e65969586cfb0fdde
olmoshbtn <2120@holbertonschool.com>
2020-10-10 23:18:46 +0000

    excercise 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
+#endif
 
 /**
  * main - program that prints its name, followed by a new line.
@@ -8,9 +16,8 @@
  * Return: print the name of the program.
  */
 
-int main(int argc,char* argv[])
+int main(int UNUSED(argc),char* argv[])
 {
         printf("%s\n",argv[0]);
-        (void)argc;
-	return 0;
+        return 0;
 }
