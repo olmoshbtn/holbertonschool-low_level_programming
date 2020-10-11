01243a97ec4c00d1a63d58af5d547a619903ed82
olmoshbtn <2120@holbertonschool.com>
2020-10-11 21:08:41 +0000

    exercise 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program that multiplies two numbers.
- * @argc: count argument
- * @argv: name argument
+ * @argc: number of arguments
+ * @argv: array containing arguments
  *
  * Return: print the result of the multiplication, followed by a new line
  */
 
 int main(int argc, char *argv[])
 {
-	int counter;
 	int mult;
 
 	if (argc < 3)
@@ -18,7 +18,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	mult = atoi (argv[1]) * atoi (argv[2]);
-        printf("%s\n", argv[counter]);
+
+	mult = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", mult);
+
 	return (0);
 }
