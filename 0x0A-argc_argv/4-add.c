22f0f3f12f90063660a80b484e7a3a4e6ba9b869
olmoshbtn <2120@holbertonschool.com>
2020-10-11 22:33:40 +0000

    exercise 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,27 +3,33 @@
 
 /**
  * main - program that adds positive numbers.
- * @argc: arguments passed to the program
- * @argv: array containing arguments
+ * @argc: number of the arguments passed to the program
+ * @argv: array of pointers to the arguments
  *
  *
- * Return: result that adds positive numbers
+ * Return: 1 number with no digit, 0 no number, result add
  */
 
 int main(int argc, char *argv[])
 {
-	int sum, i;
+	int add = 0, fs, ss;
 
-	if (no number)
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
+		for (fs = 1; fs < argc; fs++)
+		{
+			for (ss = 0; argv[fs][ss] != '\0'; ss++)
+			{
+				if (argv[fs][ss] < 48 || argv[fs][ss] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[fs]);
+		}
 	}
-	if (number with no digit)
-	{
-		printf("Error");
-		return (1);
-	}
-	sum += atoi(argv[i]);
-	printf("", );
+	printf("%d\n", add);
+
+	return (0);
 }
