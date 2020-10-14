25144abe08f9a4e36b860cc9f7794566a04868b0
olmoshbtn <2120@holbertonschool.com>
2020-10-14 19:56:59 +0000

    excercise 3 correction empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	else
 	{
