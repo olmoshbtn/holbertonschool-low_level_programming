d6338c3f3ab0e7da51594074686888e199c4fa40
olmoshbtn <2120@holbertonschool.com>
2020-10-15 19:14:25 +0000

    exercise 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,83 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+int _str_len(char *str);
+
+/**
+ * string_nconcat - pointer to a newly allocated space in memory
+ * @s1: pointer to string s1
+ * @s2: pointer to string s2
+ * @n: length to add a s1
+ *
+ * Return: NULL is passed, treat it as an empty string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len_s1, len_s2, len_sum;
+	int count, i;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+
+		len_s1 = _str_len(s1);
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+		len_s2 = _str_len(s2);
+	}
+
+	if (n == NULL)
+		n = "";
+
+       	if (n >= len_s2)
+		n = len_s2;
+
+	len_sum = len_s1 + n;
+
+	ptr = (char *) malloc(len_sum + 1) * sizeof(char));
+
+if (ptr == NULL)
+{
+	return (NULL);
+}
+
+while (count < len_s1)
+{
+	ptr[count] = s1[count];
+	count++;
+}
+while (count < len_s2)
+{
+	ptr[count] = s2[i];
+	count++;
+	i++;
+}
+s2[++i] = '\0';
+return (ptr);
+}
+
+/**
+ * _str_len - length of a string
+ * @str: pointer
+ *
+ *
+ * Return: return large of a string
+ */
+
+int _str_len(char *str)
+{
+
+	int len = 0;
+
+	while (*str)
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: the number of elements of an array
+ * @size: the number of bytes of an array
+ *
+ *
+ * Return: NULL if nmemb or size is 0, if fails NULL,
+ * otherwise a pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr =(void *) malloc(size * nmemb);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < nmemb)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
