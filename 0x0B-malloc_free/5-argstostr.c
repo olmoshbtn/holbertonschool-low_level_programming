539187cfe48218d8d8237146709a8ee118af66cf
olmoshbtn <2120@holbertonschool.com>
2020-10-15 02:45:33 +0000

    exercise 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: count arguments
+ * @av: vector arguments
+ *
+ * Return: NULL memory allocation fails, 0 if ac or av are NULL,
+ * otherwise return a pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+}
