660cfd7108a662863c856532531f45c4ebd0dddb
olmoshbtn <2120@holbertonschool.com>
2020-10-19 15:29:15 +0000

    Exercise 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - pritns the name of the file it was compiled from, by a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
+}
