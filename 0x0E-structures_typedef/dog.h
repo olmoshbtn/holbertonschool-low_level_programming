9ccffeb8c46ee3e4a26a354bdfba82815b4df55b
olmoshbtn <2120@holbertonschool.com>
2020-10-19 19:22:41 +0000

    Exercise 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef A
+#define A
+#define dog
+
+/**
+ * struct dog - definition type struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
