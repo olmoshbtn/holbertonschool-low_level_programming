3d5d7dbde21c7f1f5ca14bb22dedc2906d8f027b
olmoshbtn <2120@holbertonschool.com>
2020-10-20 02:46:01 +0000

    fix age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("Age: %.6\n", d->age ? d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
