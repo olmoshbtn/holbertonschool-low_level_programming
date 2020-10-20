d03638176707b8508dc36ee0a5c3aea46b24a947
olmoshbtn <2120@holbertonschool.com>
2020-10-20 02:37:40 +0000

    Exercise 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - unction that prints a struct dog
+ * @d: pointer to the struct dog
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age ? d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,5 @@ my_dog;
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
