9a51aadb560bb93aa2ba3b9380e7cb88b265b918
olmoshbtn <2120@holbertonschool.com>
2020-10-21 16:50:06 +0000

    Exercise 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: pointer to a name
+ * @f: pointer to a function
+ *
+ * Return: print name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
