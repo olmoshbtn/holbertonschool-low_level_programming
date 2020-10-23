ed6a9b4dd449f3fb0b53de9607f3c1d529631493
olmoshbtn <2120@holbertonschool.com>
2020-10-23 15:27:26 +0000

    Exercise 3 - Rectification error

--- 0x0F-function_pointers/3-main_copy_problems.c
+++ 0x0F-function_pointers/3-main_copy_problems.c
@@ -12,7 +12,12 @@
 
 int main(int argc, char *argv[])
 {
+<<<<<<< HEAD
+	int num1, num2;
+	char *op;
+=======
 	int call_func;
+>>>>>>> 81caf31f04ae4ec35dac4fca64487ecd3eedc122
 
 	if (argc != 4)
 	{
@@ -20,9 +25,29 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+<<<<<<< HEAD
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+=======
 	call_func = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", call_func);
+>>>>>>> 81caf31f04ae4ec35dac4fca64487ecd3eedc122
 
 	return (0);
 }
