64928d55105ef5c57eb853d44d02627ada0127b0
olmoshbtn <2120@holbertonschool.com>
2020-10-24 18:51:07 +0000

    Exercise 1 - betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (remaining--)
 	{
 		printf("%d", va_arg(arguments, int));
-		if (remaining)
+		if (separator)
 		{
 			printf("%s", separator);
 		}
