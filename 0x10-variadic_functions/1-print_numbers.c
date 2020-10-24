0292449e4adb5032cbfa3f543a4a87ddf2c61cee
olmoshbtn <2120@holbertonschool.com>
2020-10-24 18:41:53 +0000

    Exercise 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,21 +13,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	va_list list;
+	va_list args;
+	unsigned int remaining = n;
 
-	va_start(list, n);
-
-	for (i = 0; i < n; i++)
+	va_start(args, n);
+	if (!separator)
+		separator = "";
+	while (remaining--)
 	{
-		printf("%d", va_arg(list, int));
-		if (separator == NULL)
-			continue;
-		else if (i == (n - 1))
-			continue;
-		else
+		printf("%i", va_arg(args, int));
+		if (remaining)
 			printf("%s", separator);
 	}
-	va_end(list);
 	printf("\n");
+	va_end(args);
 }
