8d39f9a6d7e9c3eb69c51428f9f5526ee81bf6e9
olmoshbtn <2120@holbertonschool.com>
2020-10-24 17:27:48 +0000

    Exercise 1 - separator empty string

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -26,7 +26,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(arguments, unsigned int));
 
-		if (remaining)
+		if (remaining != 0 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
