611a3187b1e2c09bb4596b0233e43ef5dc012b73
olmoshbtn <2120@holbertonschool.com>
2020-10-24 16:59:06 +0000

    Exercise 1 - fix output n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,9 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(arguments, unsigned int));
 
 		if (remaining)
-		{
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
 	va_end(arguments);
