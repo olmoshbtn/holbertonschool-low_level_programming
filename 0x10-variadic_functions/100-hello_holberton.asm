4a330edcf4c60a4e7fd96bbbcb257b88cf952d27
olmoshbtn <2120@holbertonschool.com>
2020-10-25 23:51:35 +0000

    Exercise 100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,21 +1,18 @@
-	;; -------------------------------------------------------------------
-	;; Writes "Hello, World" to the console using only system calls.
-	;; Run on 64-bit Linux only.
-	;; To assemble and run:
-	;; nasam -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o
-	;; -o hello
-	;; ---------------------------------------------------------------------
-	global	_start
+;; -----------------------------------------------------------------------------
+;; Writes "Hola, Holberton" to the console using a C library.
+;; Runs on Linux.
+;;
+;; nasm -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o -o hello
+;;
+;; -----------------------------------------------------------------------------
 
-	section	.txt
-_start:	mov	rax, 1			;system call for write
-	mov 	rdi, 1			;file handle 1 is stdout
-	mov 	rsi, message		;address of string to output
-	mov	rsi, 16				;number of bytes
-	syscall				;invoke operating system to do the write
-	mov	rax, 60			;system call for exit
-	xor	rdi, rdi		;exit code 0
-	syscall				;invoke operating system to exit
+          global    main
+          extern    puts
 
-	section	.data
-message:db	"Hello, Holberton", 10 	;note the newline at the end
+          section   .text
+main:				; This is called by the C library startup code
+          mov       rdi, message ; First integer (or pointer) argument in rdi
+          call      puts	 ; puts(message)
+          ret			 ; Return from main back into C library wrapper
+message:
+          db        "Hola, Holberton", 0 ; Note strings must be terminated with 0 in C
