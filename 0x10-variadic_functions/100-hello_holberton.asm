9f5a44241ebf45bb9ae799259a3bfb8dff0036a9
olmoshbtn <2120@holbertonschool.com>
2020-10-26 00:34:03 +0000

    Exercise 100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,19 @@
+	section .data
+msg:		db "Hello, Holberton", 10
+msglen:		equ $-msg
+
+	section	.txt
+
 	global	main
-	extern	puts
 
-	section .text
 main:
-	mov 	rdi, message
-	call	puts
-	ret
-
-message:db	"Hello, Holberton", 0
+	mov     eax, 4
+	mov     ebx, 1
+	mov     ecx, msg
+	mov     edx, msglen
+	int	80h
+	
+	syscall		 		; invoke operating system to do the write
+	mov       rax, 60		; system call for exit
+	xor       rdi, rdi		; exit code 0
+	syscall		 		; invoke operating system to exit
