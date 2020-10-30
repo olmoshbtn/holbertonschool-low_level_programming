97863412dd9471557d67de56a3bfa7dd2583b3c8
Your Name <you@example.com>
2020-10-30 00:08:39 +0000

    Excercise 2 - add _strlen function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * _strlen - calculate the length of a string
+ * @str: the string to calculate the length of
+ *
+ * Return: the length of the string
+ */
+size_t _strlen(const char *str)
+{
+	const char *pos = str;
+
+	if (str)
+	{
+		while (*pos)
+			++pos;
+	}
+	return (pos - str);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list.
  * @head: pointer that contain address to the head pointer
  * @str: pointer that contain string by a new beginning node
@@ -15,13 +33,15 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+
 	new_nod = (list_t*) malloc(sizeof(list_t));
+
 	if (!new_nod)
 	{
 		return (NULL);
 	}
 	new_nod->str = strdup(str);
-	new_nod->len = strlen(str);
+	new_nod->len = _strlen(str);
 	new_nod->next = (*head);
 	(*head) = new_nod;
 
