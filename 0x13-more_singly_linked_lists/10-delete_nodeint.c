ec5734023ca89aec8553d1d65dbf08d9d1905771
Your Name <you@example.com>
2020-11-05 00:40:08 +0000

    Exercise 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at index of a linked list
+ * @head: pointer to a first node of the linked list
+ * @index: the index of the searched node (starting at 0)
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *aux = *head;
+	unsigned int count = 0;
+
+	if (aux != NULL)
+	{
+		while (aux != NULL && count < index)
+		{
+			count++;
+			aux = aux->next;
+		}
+		aux = (*head)->next;
+		free(*head);
+		*head = aux;
+	}
+	return (-1);
+}
