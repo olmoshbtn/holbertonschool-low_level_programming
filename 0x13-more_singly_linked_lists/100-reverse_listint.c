26d617c8be4e0a6c64e8a3a22bb367565f7396aa
olmoshbtn <2120@holbertonschool.com>
2020-11-07 19:53:58 +0000

    Exercise 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,29 +3,29 @@
 /**
  * *reverse_listint - reverse linked list
  * @head: pointer to a pointer of linked list
- * 
+ *
  * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    if (*head == NULL)
-    {
-        return (NULL);
-    }
-    if ((*head)->next != NULL)
-    {
-        listint_t *aux1 = *head;
-        listint_t *aux2 = (*head)->next;
-        (*head)->next = NULL;
-        while (aux2->next != NULL)
-        {
-            *head = aux2;
-            aux2 = (*head)->next;
-            (*head)->next = aux1;
-            aux1 = *head;
-        }
-        *head = aux2;
-        (*head)->next = aux1;
-    }
-    return (*head);
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	if ((*head)->next != NULL)
+	{
+		listint_t *aux1 = *head;
+		listint_t *aux2 = (*head)->next;
+		(*head)->next = NULL;
+		while (aux2->next != NULL)
+		{
+			*head = aux2;
+			aux2 = (*head)->next;
+			(*head)->next = aux1;
+			aux1 = *head;
+		}
+		*head = aux2;
+		(*head)->next = aux1;
+	}
+	return (*head);
 }
