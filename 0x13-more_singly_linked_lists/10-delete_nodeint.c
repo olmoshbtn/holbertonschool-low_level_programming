5a65afccf9804e2945dfe0a6a291f76caa0a9aad
Your Name <you@example.com>
2020-11-08 21:14:30 +0000

    Exercise 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *aux = (*head)->next;
 	unsigned int count = 0;
 
-	if (!head || !aux)
+	if (!(*head) || !aux)
 	{
 		return (-1);
 	}
@@ -21,10 +21,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		count++;
 		head = &(*head)->next;
-	}
-	if (!(*head))
-	{
-		return (-1);
 	}
 	aux = (*head)->next;
 	free(*head);
