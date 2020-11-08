b6061edeec9d346f3d7bce43a8d3850831d1ccb8
Your Name <you@example.com>
2020-11-08 21:20:07 +0000

    Exercise 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while ((*head)->next != NULL)
 	{
-		count--;
+		index--;
 		head = &(*head)->next;
 	}
 	if (index || !(*head))
