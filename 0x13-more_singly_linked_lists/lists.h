9d67156ad685668ba52a843d2bf121fe52351f33
Your Name <you@example.com>
2020-11-08 20:26:56 +0000

    Actualization lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -40,5 +40,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 /* 10. Delete at index */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+/* 11. Reverse list */
+listint_t *reverse_listint(listint_t **head);
 
 #endif
