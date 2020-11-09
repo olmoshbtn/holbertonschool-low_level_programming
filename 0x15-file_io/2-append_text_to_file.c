001a7b58eb174e1c6edab575106cf00730d8ced8
olmoshbtn <2120@holbertonschool.com>
2020-11-09 21:58:32 +0000

    Exercise 2 - fix Betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * create_file - function that appends text at the end of a file.
+ * append_text_to_file - function that appends text at the end of a file.
  * @filename: pointer
  * @text_content: pointer
  *
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-       		return (1);
+		return (1);
 
 	fd = open(filename, O_APPEND | O_WRONLY);
 
