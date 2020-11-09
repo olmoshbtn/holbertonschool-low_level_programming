9eed174459ccdafb9e2339634a782aa5e8a24bd7
olmoshbtn <2120@holbertonschool.com>
2020-11-09 20:03:30 +0000

    Exercise 0 - fix Betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,38 +2,39 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename:
- * @letters:
+ * @filename: name of the data
+ * @letters: number of letters
  *
- * Return: 
+ * Return: the actual number of letters it could read and print. If filename is
+ * NULL return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd, bytes;
-        char * buf;
+	int fd, bytes;
+	char *buf;
 
-        if (filename == NULL)
-        {
-                return(0);
-        }
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
-        /* read */
-        fd = open(filename, O_RDONLY);
-        if (fd == - 1)
-        {
-                return(0);
-        }
-        buf = malloc(sizeof(size_t) * letters);
-        if (buf == NULL)
-        {
-                return (0);
-        }
-        bytes = read(fd, buf, letters);
-        buf [bytes + 1] = EOF;
+	/* read */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(size_t) * letters);
+	if (buf == NULL)
+	{
+		return (0);
+	}
+	bytes = read(fd, buf, letters);
+	buf[bytes + 1] = EOF;
 
-        write(STDOUT_FILENO, buf, bytes);
+	write(STDOUT_FILENO, buf, bytes);
 
-        close(fd);
-        free(buf);
-        return (bytes); 
+	close(fd);
+	free(buf);
+	return (bytes);
 }
