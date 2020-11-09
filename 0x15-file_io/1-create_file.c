bed4c7ed07fba8c7a52447cd4eb3636dd9ca46ce
olmoshbtn <2120@holbertonschool.com>
2020-11-09 21:15:21 +0000

    Exercise 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * create_file -  function that creates a file.
+ * @filename: pointer
+ * @text_content: pointer
+ *
+ * Return: if the file already exists, truncate it.if filename is NULL return -1
+if text_content is NULL create an empty file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+		if (write(fd, text_content, i) == -1)
+		{
+			return (-1);
+		}
+	}
+	if  (close(fd) == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
