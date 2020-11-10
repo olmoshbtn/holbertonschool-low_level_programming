8b7746f4bcccdb88f313d8d57bed92643c28f651
olmoshbtn <2120@holbertonschool.com>
2020-11-10 00:54:00 +0000

    Exercise 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,23 +12,21 @@ int main(int argc, char **argv)
 	int rd_f, wr_f, fd_1, fd_2, cl_1, cl_2;
 	char buff[1024];
 
-	(argc != 3) ? dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n") :
+	if (argc != 3)
+	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-
+	}
 	fd_1 = open(argv[1], O_RDONLY);
 	rd_f = read(fd_1, buff, 1024);
 	if (rd_f == -1 || fd_1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_1);
 		exit(98);
 	}
 	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	wr_f = write(fd_2, buff, rd_f);
 	if (fd_2 == -1 || wr_f == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_2);
 		exit(99);
 	}
@@ -40,11 +38,12 @@ int main(int argc, char **argv)
 	cl_1 = close(fd_1);
 	cl_2 = close(fd_2);
 	if (cl_1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cl_1);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cl_1);
 		exit(100);
 	}
-	(cl_2 == -1) ? dprintf(STDERR_FILENO, "Error: Can't close %d\n", cl_2) :
+	if (cl_2 == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't close %d\n", cl_2);
 		exit(100);
+	}
 	return (0);
 }
