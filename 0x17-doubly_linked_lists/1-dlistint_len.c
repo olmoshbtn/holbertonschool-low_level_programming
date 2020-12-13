d69f447cf67df5e64df8cb2f85689620090d3b72
olmoshbtn <2120@holbertonschool.com>
2020-12-13 19:50:53 +0000

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,16 +3,16 @@
 /**
  * dlistint_len - number of elements in a linked dlistint_t list
  * @h: head pointer to the double linked list
- *  
- * Returns: number of elements 
+ *
+ * Returns: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t elem = 0;
+	size_t elem = 0;
 
-    for ( ; h; h = h->next)
-    {
-        ++elem;
-    }
-    return (elem);
+	for ( ; h; h = h->next)
+	{
+		++elem;
+	}
+	return (elem);
 }
