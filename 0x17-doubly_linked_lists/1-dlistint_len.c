d8c090b93d1ba5b151e9a60dca597540acc1ef4a
olmoshbtn <2120@holbertonschool.com>
2020-12-13 19:47:43 +0000

    1. List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - number of elements in a linked dlistint_t list
+ * @h: head pointer to the double linked list
+ *  
+ * Returns: number of elements 
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t elem = 0;
+
+    for ( ; h; h = h->next)
+    {
+        ++elem;
+    }
+    return (elem);
+}
