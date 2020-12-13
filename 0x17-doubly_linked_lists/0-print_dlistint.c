f5944abb2d951cd3fac218221ac7cd2602cb2424
olmoshbtn <2120@holbertonschool.com>
2020-12-13 19:10:37 +0000

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,17 +3,17 @@
 /**
  * print_dlistint - prints all elements of a dlistint_t lis.
  * @h: head pointer to the double linked list.
- * 
+ *
  * Return: the number of nodes.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    for ( ; h; h = h->next)
-    {
-        printf("%d\n", h->n);
-        ++len;
-    }
-    return (len);
+	for ( ; h; h = h->next)
+	{
+		printf("%d\n", h->n);
+		++len;
+	}
+	return (len);
 }
