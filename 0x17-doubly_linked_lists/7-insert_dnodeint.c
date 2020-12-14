a39cc8c730e79c3dbd93ceba4f5aa968f477938f
olmoshbtn <2120@holbertonschool.com>
2020-12-14 14:43:01 +0000

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,34 +6,34 @@
  * @idx: the index into which to insert the node
  * @n: data to insert in the node
  *
- * Returns:  the address of the new node, or NULL if it failed.
+ * Return:  the address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new, *tmp = *h;
+	dlistint_t *new, *tmp = *h;
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    for ( ; idx > 0; --idx)
-    {
-        tmp = tmp->next;
-            if (tmp == NULL)
-            return (NULL);
-    }
+	for ( ; idx > 0; --idx)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 
-    if (tmp->next == NULL)
-        return (add_dnodeint_end(h, n));
-    
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    
-    new->n = n;
-    new->prev = tmp;
-    new->next = tmp->next;
-    tmp->next->prev = new;
-    tmp->next = new;
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-    return (new);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
