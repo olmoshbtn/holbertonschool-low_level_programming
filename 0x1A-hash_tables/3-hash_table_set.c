f8ab3bd1096bd97d7ad999e8e65a96bd5d34afc3
olmoshbtn <2120@holbertonschool.com>
2021-01-15 21:00:49 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,19 +21,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
-	while(tmp)
+	while(tmp != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			free(tmp->value);
 			tmp->value = strdup(value);
 			return (1);
 		}
 		tmp = tmp->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	if (new_node == NULL)
+	{
+		free(new_node);
 		return (0);
+	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
