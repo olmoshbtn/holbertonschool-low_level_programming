f2f5aaee71e8e5702628e3979b22a524d5e57bec
olmoshbtn <2120@holbertonschool.com>
2021-01-28 19:14:05 +0000

    Crrection Betty

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -3,38 +3,32 @@
 /**
  * bubble_sort - sorts an array of integers in ascending order using the Bubble
  * sort algorithm
- * @array: array to be sorted  
+ * @array: array to be sorted
  * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, n, new_n;
-        int swap;
+	size_t i, n, new_n;
+	int swap;
 
-        if (array == NULL || size < 2)
-            return;
-        
-        n = size;
-        while (n > 0)
-        {
-            new_n = 0;
-            for (i = 0; i < n -1; i++)
-            {
-                if (array[i] > array[i + 1])
-                {
-                    swap = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = swap;
-                    new_n = i + 1;
-                    print_array(array, size);
-                }
-            }
-            n = new_n;
-        }
-        
-        {
-            /* code */
-        }
-        
+	if (array == NULL || size < 2)
+		return;
 
+	n = size;
+	while (n > 0)
+	{
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
+				print_array(array, size);
+			}
+		}
+		n = new_n;
+	}
 }
